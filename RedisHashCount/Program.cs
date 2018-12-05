4dcba3d7a4c080ad38ee0b4ac21fb5db06bf18bb
cbossie <cbossie@gmail.com>
2018-12-05 15:12:15 -0500

    Add project files.

---
+++ RedisHashCount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisHashCount
+{
+    class Program
+    {
+        private static string FileName = $"_{ConfigurationManager.AppSettings["FileName"]}{DateTime.Now:yyyyMMddHHmmss}.txt";
+
+        static void Main(string[] args)
+        {
+            var ctr = new RedisCounter(ConfigurationManager.AppSettings["RedisConnection"]);
+            var results = ctr.GetHashCounts(ConfigurationManager.AppSettings["ClientKeyPrefix"]);
+
+
+            var items = results.OrderBy(a => a.Key).Select(b => $"{b.Key}\t{b.Value.ToString()}").ToList();
+
+
+            items.Insert(0, "FirmID\tClientCount");
+            
+            File.WriteAllLines(FileName, items);
+
+
+
+
+        }
+    }
+}
---
+++ RedisHashCount/RedisCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+using StackExchange.Redis.Extensions.Core;
+using StackExchange.Redis.Extensions.Core.Configuration;
+using StackExchange.Redis.Extensions.Newtonsoft;
+
+namespace RedisHashCount
+{
+    public class RedisCounter
+    {
+        private StackExchangeRedisCacheClient Client { get; }
+        
+        public RedisCounter(string connStr)
+        {            
+            Client = new StackExchangeRedisCacheClient(ConnectionMultiplexer.Connect(connStr), new NewtonsoftSerializer());
+        }
+
+        public Dictionary<string, string> GetHashCounts(string prefix)
+        {
+            Dictionary<string, string> results = new Dictionary<string, string>();
+
+            //1) Get Keys
+            var keys = Client.SearchKeys($"{prefix}*");
+
+            //2) For each key, get the raw firm ID
+            foreach (var key in keys)
+            {
+                var label = key.Replace(prefix, string.Empty);
+                var lengths = Client.HashLength(key);
+                results[label] = lengths.ToString();
+            }
+            return results;
+        }
+
+
+
+
+    }
+
+
+}
